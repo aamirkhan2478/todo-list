27d52847a2ede5bcfcce289010ba1f2b2cada87d
Aamir Khan <aamir.khan2478@gmail.com>
2022-08-12 15:41:57 +0500

    removed src

--- src/modules/getElements.js
+++
@@ -1,4 +0,0 @@
-export const data = document.getElementById("items");
-export const textBox = document.getElementById("textBox");
-export const clearCompletedTasks = document.getElementById("clear-task");
-export const errorMessage = document.getElementById("error");
--- src/modules/listObj.js
+++
@@ -1,9 +0,0 @@
-class List {
-  constructor(index, description) {
-    this.index = index;
-    this.description = description;
-    this.completed = false;
-  }
-}
-
-export default List;
